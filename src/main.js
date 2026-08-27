5b707a283aa6a57f3d0f590966eee382e3c15419
Raazim tech <raazimtech@gmail.com>
2026-08-27 11:17:51 +0300

    Simplify app bootstrap and remove duplicate navigation injector

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
-import './navigation-dock.css'
-import { installNavigationDock } from './navigation-dock.js'
 
-const app=createApp(App)
-app.mount('#app')
-installNavigationDock(app)
+createApp(App).mount('#app')
